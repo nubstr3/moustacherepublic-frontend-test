3894f6611a2a68f55c94fa715333d71c3176974a
Henry Li <nubstr3@live.com>
2024-05-07 18:57:23 +1200

    API breakdown

--- product-test-app/src/App.js
+++ product-test-app/src/App.js
@@ -1,24 +1,66 @@
 import React from 'react';
-import {useEffect } from 'react';
+import {useEffect, useState} from 'react';
+import './App.css';
 
 const API_URL = 'https://3sb655pz3a.execute-api.ap-southeast-2.amazonaws.com/live/product';
 
 const App = () => {
 
+    const [product1, setProduct1] = useState([]);
+    const [sizes, setSizes] = useState([]);
+    const [size, setSize] = useState(-1);
+
     const productData = async() => {
         const response = await fetch(API_URL);
         const data = await response.json();
+        
+        setProduct1(data)
+        setSizes(data.sizeOptions)
+        console.log(data)
 
-        console.log(data);
     }
-
     useEffect(() => {
         productData();
 
     }, []);
-
+    
     return (
-        <h1>App</h1>
+        <div className='app'>
+            <h1 className='EcommerceName'>Ecommerce Name</h1>
+
+            <container className='navbar'>
+
+            </container>
+
+            <div className='productPage'>
+                <h2 className='title'>
+                    {product1.title}
+                </h2>
+
+                <container className='productImage'>
+                    <img src={product1.imageURL} alt='product picture'></img>
+                    
+                </container>
+                <container className='productDescription'>
+                    <p>
+                        {product1.description}
+                    </p>
+
+                </container>
+                <container className='productSizeOptions'>
+                    {sizes.map((size) => 
+                        <button className='sizeButtons'>{size.label}</button>
+                    ) }
+
+                </container>
+                <container className='addToCart'>
+                    <button className='addToCartButton'>Add to Cart</button>
+                </container>
+
+            </div>
+
+
+        </div>
     );
 }
 
